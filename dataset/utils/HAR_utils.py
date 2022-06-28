bff03054318de09bf8abfec440248759634982b7
Tsing <2719584131@qq.com>
2022-06-28 12:10:49 +0800

    Update HAR_utils.py

--- dataset/utils/HAR_utils.py
+++ dataset/utils/HAR_utils.py
@@ -48,10 +48,10 @@ def split_data(X, y):
         unique, count = np.unique(y[i], return_counts=True)
         if min(count) > 1:
             X_train, X_test, y_train, y_test = train_test_split(
-                X[i], y[i], train_size=train_size, shuffle=False)
+                X[i], y[i], train_size=train_size, shuffle=True)
         else:
             X_train, X_test, y_train, y_test = train_test_split(
-                X[i], y[i], train_size=train_size, shuffle=False)
+                X[i], y[i], train_size=train_size, shuffle=True)
 
         train_data.append({'x': X_train, 'y': y_train})
         num_samples['train'].append(len(y_train))
